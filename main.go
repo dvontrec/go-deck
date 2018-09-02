60b8c12c56b2cc87c1754b411d98bf19494225a1
Dvontre Coleman <dvontrec@gmail.com>
2018-09-02 12:50:30 -0400

    Create first function and variable

---
+++ main.go
@@ -0,0 +1,11 @@
+// Tells that this is an exacutable file
+package main
+
+import "fmt"
+
+// Function that is run when main is run.
+func main() {
+	// Creates a new infered string variable called card
+	card := "Ace of Spades"
+	fmt.Println(card)
+}

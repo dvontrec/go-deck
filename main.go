ebcbe20f8690e4e5bcfb5681ac014cfe9266fb69
Dvontre Coleman <dvontrec@gmail.com>
2018-09-02 15:30:14 -0400

    Setup arrays and loops

--- main.go
+++ main.go
@@ -5,9 +5,15 @@ import "fmt"
 
 // Function that is run when main is run.
 func main() {
-	// Creates a new infered string variable called card
-	card := newCard()
-	fmt.Println(card)
+	// Creates a dynamic sized array(SLICE) of strings
+	cards := []string{newCard(), newCard()}
+	// creates a new array with the new card appended to the cards array
+	cards = append(cards, newCard())
+
+	// Loops through each element in the cards array as card
+	for i, card := range cards {
+		fmt.Println(i, card)
+	}
 }
 
 // Defines a function that returns a string

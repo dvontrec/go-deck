994752ec777dd02e3e197b755fd764d6865eb376
Dvontre Coleman <dvontrec@gmail.com>
2018-09-05 12:51:58 -0400

    Complete deck creation method

--- deck.go
+++ deck.go
@@ -5,6 +5,22 @@ import "fmt"
 // Create a new type of deck that is a slica of strings
 type deck []string
 
+// Constructor function that returns a new deck of cards
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Hearts", "Diamonds", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Jack", "Queen", "King"}
+
+	// Loops through the suits and values to create the appropriate card
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+	return cards
+}
+
 // Function that loops through the deck and prints out the values
 func (d deck) print() {
 	// loops through the deck and prints out the values
--- main.go
+++ main.go
@@ -4,15 +4,8 @@ package main
 // Function that is run when main is run.
 func main() {
 	// Creates a dynamic sized array(SLICE) of strings
-	cards := deck{newCard(), newCard()}
-	// creates a new array with the new card appended to the cards array
-	cards = append(cards, "Six of diamonds")
+	cards := newDeck()
 
 	// calls the print method on the cards deck
 	cards.print()
-}
-
-// Defines a function that returns a string
-func newCard() string {
-	return "Five of Diamonds"
 }

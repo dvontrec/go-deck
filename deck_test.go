33713b2befe305fabfb7266a6e58ea6a63a4ac72
Dvontre Coleman <dvontrec@gmail.com>
2018-09-09 15:22:22 -0400

    Create test for saving and loading from hard drive

--- deck_test.go
+++ deck_test.go
@@ -1,6 +1,9 @@
 package main
 
-import "testing"
+import (
+	"os"
+	"testing"
+)
 
 // Tests  the NewDeck function
 func TestNewDeck(t *testing.T) {
@@ -18,4 +21,21 @@ func TestNewDeck(t *testing.T) {
 	if d[len(d)-1] != "King of Clubs" {
 		t.Errorf("Expected Four of Clubs, but got %v.", d[len(d)-1])
 	}
+}
+
+// Tests saving and loading deck on harddrive
+func TestSaveToDeckAndNewDeckFromFile(t *testing.T) {
+	// removes old test files
+	os.Remove("_decktesting")
+
+	deck := newDeck()
+	deck.saveToFile("_decktesting")
+	loadedDeck := newDeckFromFile("_decktesting")
+
+	// Checks that the deck was properly saved and can be loaded
+	if len(loadedDeck) != 52 {
+		t.Errorf("Expected a 52 card deck, but got %v", len(loadedDeck))
+	}
+
+	os.Remove("_decktesting")
 }
